1ef936a24a05ef32521501b4d1d188c26c6a29c9
VanCleef <vancleefy@protonmail.com>
2022-06-02 21:51:44 +0300

    added for.sol

---
+++ solidity workfolder/solidity-templates/Template-For.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
+//Remove the /* and */ tags to de-comment the code
+/*
+contract forContract {
+
+    function name () keywords {
+        //best used with a counter
+        uint count = 0;  
+        
+        for(uint i=1; i < 10; i++){
+        
+            count++;
+        
+        }
+    }
+
+*/

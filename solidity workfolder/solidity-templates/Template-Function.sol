9fbf0a4dcf7b2287e9fc3b67443f3ab8d6de52a7
VanCleef <vancleefy@protonmail.com>
2022-06-02 19:56:11 +0300

    cleanup §

---
+++ solidity workfolder/solidity-templates/Template-Function.sol
@@ -0,0 +1,10 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+//contract "contract name" {
+
+    function "function name" ("type of data (ex: uint)" "variable name (ex: _nr)", "second parameter", "etc.")
+     "keywords (ex:public,view,returns(\"type of data to return\")" {
+
+    }
+
+//}
